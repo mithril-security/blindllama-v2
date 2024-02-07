a613d464640473136832c96404629960b862a2bf
yass <bargach.y@gmail.com>
2024-02-07 19:01:34 +0000

    lfs

--- lfs_update.sh
+++ lfs_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update && apt install git-lfs
+apt update && apt install git-lfs jq
 
-curl -x POST -H  "Content-Type: application/json" -H "Authorization: Bearer $(gcloud auth print-access-token)" https://cloudbuild.googleapis.com/v2/projects/amd-sev-356012/locations/europe-west1/connections/blindllama-v2/repositories/mithril-security-blindllama-v2:accessReadToken
+GITHUB_TOKEN=$(curl -X POST -H  "Content-Type: application/json" -H "Authorization: Bearer $(gcloud auth print-access-token)" https://cloudbuild.googleapis.com/v2/projects/amd-sev-356012/locations/europe-west1/connections/blindllama-v2/repositories/mithril-security-blindllama-v2:accessReadToken | jq .token)
 
-git lfs pull 
+GIT_LFS_SKIP_SMUDGE=1 git -c credential.helper= -c "credential.https://github.com/.helper=!f() { echo password=$GITHUB_TOKEN; }" lfs pull
