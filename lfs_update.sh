d7b9171d0c6e8db41858d61e5f21b6b7cc0ca22e
yass <bargach.y@gmail.com>
2024-02-07 19:16:22 +0000

    lfs

--- lfs_update.sh
+++ lfs_update.sh
@@ -3,6 +3,11 @@
 apt -y update && apt install -y git-lfs jq
 
 GITHUB_TOKEN=$(curl -X POST -H  "Content-Type: application/json" -H "Authorization: Bearer $(gcloud auth print-access-token)" https://cloudbuild.googleapis.com/v2/projects/amd-sev-356012/locations/europe-west1/connections/blindllama-v2/repositories/mithril-security-blindllama-v2:accessReadToken | jq .token)
+USERNAME=mithril-security
 
+echo $USERNAME > creds
+echo $GITHUB_TOKEN >> creds
 
-GIT_LFS_SKIP_SMUDGE=1 git -c credential.helper= -c "credential.https://github.com/.helper=!f() { echo password=$GITHUB_TOKEN; }" lfs pull
+git config --global credential.helper "./creds"
+
+GIT_LFS_SKIP_SMUDGE=1 git lfs pull
