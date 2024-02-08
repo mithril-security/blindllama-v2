c7cc1186920f996ce9dfcb71c8fa726f1064f3c4
yass <bargach.y@gmail.com>
2024-02-08 09:51:10 +0000

    q

--- lfs_update.sh
+++ lfs_update.sh
@@ -2,11 +2,11 @@
 
 apt -y update && apt install -y git-lfs jq
 
-GITHUB_TOKEN=$(curl -X POST -H  "Content-Type: application/json" -H "Authorization: Bearer $(gcloud auth print-access-token)" https://cloudbuild.googleapis.com/v2/projects/amd-sev-356012/locations/europe-west1/connections/blindllama-v2/repositories/mithril-security-blindllama-v2:accessReadToken | jq .token)
-gcloud auth print-access-token
+# GITHUB_TOKEN=$(curl -X POST -H  "Content-Type: application/json" -H "Authorization: Bearer $(gcloud auth print-access-token)" https://cloudbuild.googleapis.com/v2/projects/amd-sev-356012/locations/europe-west1/connections/blindllama-v2/repositories/mithril-security-blindllama-v2:accessReadToken | jq .token)
+# gcloud auth print-access-token
 
-gcloud auth list 
+# gcloud auth list 
 # gcloud builds connections describe blindllama-v2 --region=europe-west1
-git remote remove origin
-git remote add origin https://$GITHUB_TOKEN@github.com/mithril-security/blindllama-v2.git
+# git remote remove origin
+# git remote add origin https://$GITHUB_TOKEN@github.com/mithril-security/blindllama-v2.git
 GIT_LFS_SKIP_SMUDGE=1 git lfs pull
