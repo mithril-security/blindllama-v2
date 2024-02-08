a2fb348f61b76ee1716c8744d6e572763e1113ae
yass <bargach.y@gmail.com>
2024-02-08 10:45:57 +0000

    lfs

--- lfs_update.sh
+++ lfs_update.sh
@@ -8,5 +8,5 @@ apt -y update && apt install -y git-lfs jq
 # gcloud auth list 
 # gcloud builds connections describe blindllama-v2 --region=europe-west1
 # git remote remove origin
-# git remote add origin https://$GITHUB_TOKEN@github.com/mithril-security/blindllama-v2.git
+git remote add origin https://$AUTH_TOKEN@github.com/mithril-security/blindllama-v2.git
 GIT_LFS_SKIP_SMUDGE=1 git lfs pull
